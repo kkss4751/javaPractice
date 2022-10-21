9d475a051ed73a1273e3fe166b663f6f849c8523
Jeonghun-Lee <kkss4751@naver.com>
2022-10-21 19:13:06 +0900

    10/21 CodeUp1094 이상한 출석 번호 부르기2

--- main/java/com/example/javapractice/codeUp/CodeUp1093_1095.java
+++ main/java/com/example/javapractice/codeUp/CodeUp1093_1095.java
@@ -19,3 +19,16 @@ public class CodeUp1093_1095 {
         }
     }
 }
+
+/** CodeUp1094 이상한 출석 번호 부르기2*/
+class CodeUp1094{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int [] arr = new int[a];
+        for (int i = 0; i < a; i++){
+            arr[i] = sc.nextInt();
+        }
+        for (int i = a -1; i>0; i--) System.out.println(arr[i] + "");
+    }
+}
